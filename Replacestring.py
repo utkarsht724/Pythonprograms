3be6617ba4292151ca0e56039d5cb55f45b134ed
Utkarsht724 <utkarsht724@gmail.com>
2020-02-07 10:03:37 +0530

    Added:Replace string

---
+++ Replacestring.py
@@ -0,0 +1,6 @@
+#replaceUSERNAMEWITH
+print("Original string : Hello USERNAME ,How are you ?")
+str = "New String      : Hello USERNAME ,How are you ?"
+name=input("enter the name you want to replace with USERNAME :")
+result = str.replace("USERNAME", name)
+print(result)
