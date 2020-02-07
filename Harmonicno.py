1b496764e3674cab1ad34b89acef015c896e38fa
Utkarsht724 <utkarsht724@gmail.com>
2020-02-07 11:21:11 +0530

    Added: Harmonic program

---
+++ Harmonicno.py
@@ -0,0 +1,8 @@
+#program to display nth harmonic value
+
+n=int(input("enter the value of n"))    #to take value of n from the user
+
+
+for i in range (1,n):        #iterate n times from 1/1 to 1/n
+
+    print(1,"/",i)
