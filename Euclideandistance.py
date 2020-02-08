9dfb3272f2fed0ae9f63dfa823d8641c2d60823e
Utkarsht724 <utkarsht724@gmail.com>
2020-02-08 09:03:23 +0530

    Added: Euclidean Distance program

--- Euclideandistance.py
+++ Euclideandistance.py
@@ -1,6 +1,6 @@
 # program to find Eucledian Distance
 import math
-x=int(input("enter the value of x="))  #taking input from the user
-y=int(input("enter the value of y="))  #taking input from the user
-distance=math.sqrt(x*x+y*y)
+x=int(input("enter the value of x="))  #taking first value from the user
+y=int(input("enter the value of y="))  #taking second value from the user
+distance=math.sqrt(x*x+y*y)            #applying formula to find distance
 print(distance)
