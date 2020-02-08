3813f53d0911335c546b9a2d30502dc199fa39ba
Utkarsht724 <utkarsht724@gmail.com>
2020-02-08 15:02:55 +0530

    Refactored: used regex for string operations

--- Replacestring.py
+++ Replacestring.py
@@ -1,6 +1,9 @@
-#replaceUSERNAMEWITH
-print("Original string : Hello USERNAME ,How are you ?")
-str = "New String      : Hello USERNAME ,How are you ?"
-name=input("enter the name you want to replace with USERNAME :")
-result = str.replace("USERNAME", name)
-print(result)
+#program to replace USERNAME  with any name in a string
+
+import re
+str= print("Hello USERNAME How are you?")
+name=input("Enter the name you want to replace with USERNAME  :")  #taking input name from the user
+str ="Hello USERNAME How are you?"
+regex =re.compile("USERNAME")
+str = regex.sub(name,str)               #replace Username with input name by using regular expression
+print(str)
