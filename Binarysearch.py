810e4f18ff1c00d1f1607702b687a9dd61b02f2a
utkarsh <utkarsht724@gmail.com>
2020-02-12 11:42:30 +0530

    Added:Binary search

--- Binarysearch.py
+++ Binarysearch.py
@@ -1,24 +1,29 @@
-from array import *
+#binary search the word from word list
 def binarySearch(arr,x):
-    l=0
-    r=len(arr)
-    while(l<=r):
-       m=l+((r-l)/2)
-       res=(x==arr[m])
-       if res==0:
-         return m-1
-         print("element found")
-       elif res>0:
-          l=m+1
+    l = 0
+    r = len(arr)
+    while(l <= r):
+       m = l + ((r - l) // 2)
+
+       res = (x == arr[m])
+
+       if res == 0:
+         return m - 1
+
+       if res > 0:
+          l = m + 1
+
+       else:
+        r = m-1
+
+    return-1
+
+if __name__ == '__main__':
+    arr=["utkarsh","ananya","shambhavi","ashu"]
+    x=input("enter the name you want to search")
+    result= binarySearch(arr,x)
+
+    if result == -1:
+      print("element is not present")
     else:
-       r=m-1
-       return -1
-
-
-arr=["utkarsh","ananya","shambhavi","ashu"]
-x=input("enter the word u want to search")
-result = binary search(arr,x)
-if(result==-1)
-    print("element is not present")
-else
-    print("element found",result)
+      print("element found",result)
